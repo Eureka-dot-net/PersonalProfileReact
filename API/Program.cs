091c81ac4d2263521e703ac7aa6e8d57e4ca0c41
Eureka <narike@gmail.com>
2025-07-25 11:47:39 +0300

    Retry sql connection on failure

--- API/Program.cs
+++ API/Program.cs
@@ -18,7 +18,8 @@ builder.Services.AddControllers()
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+        , sqlOptions => sqlOptions.EnableRetryOnFailure()));
 
 var app = builder.Build();
 
